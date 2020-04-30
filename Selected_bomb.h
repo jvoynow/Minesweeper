50b94704c7e2404abe1043c2ca8f7986f6c40c0b
Parker S <59847425+pstrawbr@users.noreply.github.com>
2020-04-29 23:25:21 -0400

    Basic Bomb drawing

--- Selected_bomb.h
+++ Selected_bomb.h
@@ -12,8 +12,7 @@ public:
 
     void draw() const override {
         // TODO: Change this
-        //glColor3f(current_fill.r,current_fill.g,current_fill.b);
-        glColor3f(0,0,1);
+        glColor3f(current_fill.r, current_fill.g, current_fill.b);
         glBegin(GL_QUADS);
         glVertex2i(c1, r1);
         glVertex2i(c2, r1);
@@ -22,15 +21,47 @@ public:
         glEnd();
 
 
-//        // TODO: Draw Bomb here
-//        unsigned char filler[] = "10";
-//        int num = 10;
-//        glColor3f(0,0,0);
-//        glRasterPos2i(c1 + (0.5 * (c2 - c1)) - (0.5 * glutBitmapLength(GLUT_BITMAP_HELVETICA_18, filler)),
-//                      r1 + (0.5 * (r2 - r1)) + (0.75 * glutBitmapLength(GLUT_BITMAP_HELVETICA_18, filler)));
-//        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, reinterpret_cast<int>('0' + num));
-//        glEnd();
+        glColor3f(0, 0, 0);
+        glBegin(GL_QUADS);
+        glVertex2i((c2 - c1) / 2 + 1.5 + c1, (r2 - r1) / 6 + r1);
+        glVertex2i((c2 - c1) / 2 - 1.5 + c1, (r2 - r1) / 6 + r1);
+        glVertex2i((c2 - c1) / 2 - 1.5 + c1, r2 - ((r2 - r1) / 6));
+        glVertex2i((c2 - c1) / 2 + 1.5 + c1, r2 - ((r2 - r1) / 6));
+        glEnd();
+
+        glColor3f(0, 0, 0);
+        glBegin(GL_QUADS);
+        glVertex2i((c2 - c1) / 4 + c1, (r2 - r1) / 2 + 1.5 + r1);
+        glVertex2i(c2 - ((c2 - c1) / 4), (r2 - r1) / 2 + 1.5 + r1);
+        glVertex2i(c2 - ((c2 - c1) / 4), (r2 - r1) / 2 - 1.5 + r1);
+        glVertex2i((c2 - c1) / 4 + c1, (r2 - r1) / 2 - 1.5 + r1);
+        glEnd();
+
+
+        glColor3f(0, 0, 0);
+        glBegin(GL_TRIANGLE_FAN);
+        glVertex2i((c2 - c1) / 2 + c1, (r2 - r1) / 2 + r1);
+
+        glVertex2i((c2 - c1) / 2 + c1, (r2 - r1) / 4 + r1);
+        glVertex2i((c2 - c1) / 2.5 + c1, (r2 - r1) / 3 + r1);
+        glVertex2i((c2 - c1) / 3 + c1, (r2 - r1) / 2 + r1);
+        glVertex2i((c2 - c1) / 2.5 + c1, r2 - ((r2 - r1) / 3));
+        glVertex2i((c2 - c1) / 2 + c1, r2 - ((r2 - r1) / 4));
+        glVertex2i(c2 - ((c2 - c1) / 2.5), r2 - ((r2 - r1) / 3));
+        glVertex2i(c2 - ((c2 - c1) / 3), (r2 - r1) / 2 + r1);
+        glVertex2i(c2 - ((c2 - c1) / 2.5), (r2 - r1) / 3 + r1);
+        glVertex2i((c2 - c1) / 2 + c1, (r2 - r1) / 4 + r1);
+        glEnd();
+
+        glColor3f(1.0,1.0,1.0);
+        glBegin(GL_QUADS);
+        glVertex2i((c2 - c1) / 2.3 + c1, (r2 - r1) / 2.5 + r1);
+        glVertex2i((c2 - c1) / 2 + c1, (r2 - r1) / 2.5 + r1);
+        glVertex2i((c2 - c1) / 2 + c1, (r2 - r1) / 2.1 + r1);
+        glVertex2i((c2 - c1) / 2.3 + c1, (r2 - r1) / 2.1 + r1);
+        glEnd();
     }
+
 };
 
 #endif //PS_JV_GRAPHICS_FINAL_SELECTED_BOMB_H
